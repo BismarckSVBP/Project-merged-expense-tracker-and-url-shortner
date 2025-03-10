a77d22d29421285549b47247fd1f9266e162514e
BismarckSVBP <163289874+BismarckSVBP@users.noreply.github.com>
2025-03-11 03:41:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,39 +2,29 @@ const express = require("express");
 const path = require("path");
 const { connectToMongoDB } = require("./connections");
 
-
-
 const otp = require('./routes/otp')
 const signUp = require("./routes/signUp");
 const login = require("./routes/login");
 const expense = require("./routes/expenseui");
 const afterlogin = require("./routes/afterlogin");
 const UI = require("./models/expenseui");
-
 const cookieParser = require("cookie-parser");
 const { restrictToLoggedinUserOnly, checkAuth} = require("./middlewares/foraccessingafterloginpage");
 const app = express();
-
-const sleeptracker = require("./routes/sleeptracker");
 const homepage = require("./routes/homepage");
 
-
 const port = 1002; 
-connectToMongoDB("mongodb+srv://abhay:abhay@cluster0.nmhaanc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0").then(() =>
+connectToMongoDB("mongodb://localhost:27017/").then(() =>
   console.log("MongoDb connected")
 );
 
 app.set("view engine", "ejs");
 app.set("views", path.resolve("./view"));
 
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-
-
- 
 app.use("/",signUp );
 app.use("/auth",otp);
 
@@ -43,7 +33,6 @@ app.use("/user/expense",restrictToLoggedinUserOnly,checkAuth,expense);
 
 app.use("/user/afterloginpage",restrictToLoggedinUserOnly,afterlogin);
 
-app.use("/user/sleeptracker",restrictToLoggedinUserOnly,sleeptracker);
 
 app.use("/homepage",homepage);
 
@@ -51,16 +40,6 @@ app.use(express.static(path.join(__dirname, '../public')));
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
-
-
-
-
-
-
-
-
-
-
 
 const urlRoute = require("./routes/url");
 
@@ -88,10 +67,6 @@ app.get("/url/:shortId", async (req, res) => {
   );
   res.redirect(entry.redirectURL);
 });
-
-
-
-
 
 
 app.listen(port, () => console.log(`Server Started at PORT:${port}`));
