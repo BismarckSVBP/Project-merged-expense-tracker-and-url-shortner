bad772eedb285209cdabd5a11ac887837feb6025
Abhay Kumar <163289874+BismarckSVBP@users.noreply.github.com>
2025-04-03 15:11:38 +0530

    Update connections.js

--- connections.js
+++ connections.js
@@ -1,9 +1,17 @@
 const mongoose = require("mongoose");
+require("dotenv").config();
 
-async function connectToMongoDB(url) {
-  return mongoose.connect(url);
-}
-
-module.exports = {
-  connectToMongoDB,
+const connectToMongoDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("✅ MongoDB Connected Successfully!");
+  } catch (error) {
+    console.error("❌ MongoDB Connection Error:", error);
+    process.exit(1); // Exit on failure
+  }
 };
+
+module.exports = { connectToMongoDB };
